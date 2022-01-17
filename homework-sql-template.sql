9df64befdfe277320f2bc0388342dceea5da680b
Andrew Kara <67162220+tatakaeq@users.noreply.github.com>
2022-01-17 12:55:52 +0200

    Add files via upload

---
+++ homework-sql-template.sql
@@ -0,0 +1,80 @@
+﻿--------------------------ДОМАШКА----------------------------
+
+----------------------ЗАДАНИЕ №1-----------------------------
+--Вывести всю информацию из таблицы Sales.Customer 
+-------------------------------------------------------------
+
+GO
+select * from Sales.Customer
+----------------------ЗАДАНИЕ №2-----------------------------
+--Вывести всю информацию из таблицы Sales.Store отсортированную 
+--по Name в алфавитном порядке
+-------------------------------------------------------------
+
+GO
+select * from Sales.Store order by Name
+----------------------ЗАДАНИЕ №3-----------------------------
+--Вывести из таблицы HumanResources.Employee всю информацию
+--о десяти сотрудниках, которые родились позже 1989-09-28
+-------------------------------------------------------------
+
+GO
+select top 10 * from HumanResources.Employee where BirthDate>'1989-09-28' 
+----------------------ЗАДАНИЕ №4-----------------------------
+--Вывести из таблицы HumanResources.Employee сотрудников
+--у которых последний символ LoginID является 0.
+--Вывести только NationalIDNumber, LoginID, JobTitle.
+--Данные должны быть отсортированы по JobTitle по убиванию
+-------------------------------------------------------------
+
+GO
+select NationalIDNumber, LoginID, JobTitle from HumanResources.Employee where LoginID like '%0' order by JobTitle desc
+----------------------ЗАДАНИЕ №5-----------------------------
+--Вывести из таблицы Person.Person всю информацию о записях, которые были 
+--обновлены в 2008 году (ModifiedDate) и MiddleName содержит
+--значение и Title не содержит значение 
+-------------------------------------------------------------
+
+GO
+select * from Person.Person where DATEPART(YY, ModifiedDate)=2008 and MiddleName is not null and Title is null
+----------------------ЗАДАНИЕ №6-----------------------------
+--Вывести название отдела (HumanResources.Department.Name) БЕЗ повторений
+--в которых есть сотрудники
+--Использовать таблицы HumanResources.EmployeeDepartmentHistory и HumanResources.Department
+-------------------------------------------------------------
+
+GO
+select distinct(select d.Name from HumanResources.Department d where d.DepartmentID = e.DepartmentID) as DepartmentName from HumanResources.EmployeeDepartmentHistory e
+----------------------ЗАДАНИЕ №7-----------------------------
+--Сгрупировать данные из таблицы Sales.SalesPerson по TerritoryID
+--и вывести сумму CommissionPct, если она больше 0
+-------------------------------------------------------------
+
+GO
+select TerritoryID, SUM(CommissionPct) as CommissionPctSum from Sales.SalesPerson group by TerritoryID having SUM(CommissionPct) > 0   
+----------------------ЗАДАНИЕ №8-----------------------------
+--Вывести всю информацию о сотрудниках (HumanResources.Employee) 
+--которые имеют самое большое кол-во 
+--отпуска (HumanResources.Employee.VacationHours)
+-------------------------------------------------------------
+
+GO
+select * from HumanResources.Employee where VacationHours = (select MAX(VacationHours) from HumanResources.Employee)
+----------------------ЗАДАНИЕ №9-----------------------------
+--Вывести всю информацию о сотрудниках (HumanResources.Employee) 
+--которые имеют позицию (HumanResources.Employee.JobTitle)
+--'Sales Representative' или 'Network Administrator' или 'Network Manager'
+-------------------------------------------------------------
+
+GO
+select * from HumanResources.Employee where JobTitle='Sales Representative' or JobTitle='Network Administrator' or JobTitle='Network Manager'
+----------------------ЗАДАНИЕ №10-----------------------------
+--Вывести всю информацию о сотрудниках (HumanResources.Employee) и 
+--их заказах (Purchasing.PurchaseOrderHeader). ЕСЛИ У СОТРУДНИКА НЕТ
+--ЗАКАЗОВ ОН ДОЛЖЕН БЫТЬ ВЫВЕДЕН ТОЖЕ!!!
+-------------------------------------------------------------
+
+
+GO
+select * from HumanResources.Employee e left join Purchasing.PurchaseOrderHeader p on e.BusinessEntityID = p.EmployeeID
+
